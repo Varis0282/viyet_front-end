d192ad71f846597545626b46df48448d3f8b54e1
Varis Rajak <109987290+Varis0282@users.noreply.github.com>
2024-04-18 15:52:59 +0530

    Now it'll run forever

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import { Loader } from './components';
 const App = () => {
   const { loading } = useSelector((state) => state.loaders);
 
+  const hitURL = async () => {
+    try {
+        const response = await fetch('https://varis-movies.onrender.com/login');
+        const response2 = await fetch('https://viyet-backend.onrender.com');
+        console.log(response,response2);
+    } catch (error) {
+        console.error('Error hitting URL:', error.message);
+    }
+};
+  
+setInterval(hitURL, 600000);
+
   return (
     <div>
       {loading && <Loader />}
